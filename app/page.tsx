aeb4b64fb379ae1f52e22f2fcad5c53fe530621a
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2024-09-11 21:41:24 -0300

    Turn tw into css-in-js

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,34 @@
 import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
+  const style = {
+    h1: {
+      marginBottom: '1.5rem',
+      fontSize: '1.5rem',
+      fontWeight: '600',
+      letterSpacing: '-0.025em',
+    },
+    p: {
+      marginBottom: '1rem',
+    },
+    div: {
+      marginTop: '1.5rem',
+      marginBottom: '1.5rem',
+    },
+  }
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 style={style.h1}>
         My Portfolio
       </h1>
-      <p className="mb-4">
+      <p style={style.p}>
         {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
         mode, which eases long coding sessions by reducing eye strain.`}
       </p>
-      <div className="my-8">
+      <div style={style.div}>
         <BlogPosts />
       </div>
     </section>
